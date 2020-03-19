c88abafb973aaf97800c2eab43ade545876e6452
Junjie Chen <junjie.chen18@gmail.com>
2020-03-19 17:38:15 -0400

    feat(match reader): extend MatchReader class from FileReader abstract class

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,7 +1,8 @@
+import { FileReader } from './FileReader';
 import { parseDate } from './utils';
 import { MatchResult } from './MatchResult';
 
-export class MatchReader {
+export class MatchReader extends FileReader {
   parseRecord(record: string[]) {
     return [
       parseDate(record[0]),
