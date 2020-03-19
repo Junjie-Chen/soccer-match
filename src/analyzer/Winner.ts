cd1f7ec98145790fb4d8b5898f9f902df67bb6fb
Junjie Chen <junjie.chen18@gmail.com>
2020-03-19 17:59:25 -0400

    feat(analyzer): define analyze method

--- src/analyzer/Winner.ts
+++ src/analyzer/Winner.ts
@@ -1,3 +1,20 @@
+import { MatchType } from '../MatchType';
+import { MatchResult } from '../MatchResult';
+
 export class Winner {
   constructor(public team: string) {}
+
+  analyze(matches: MatchType[]): string {
+    let winningGames = 0;
+
+    for (let match of matches) {
+      if (match[1] === this.team && match[5] === MatchResult.HomeTeamWinsTheGame) {
+        winningGames++;
+      } else if (match[2] === this.team && match[5] === MatchResult.AwayTeamWinsTheGame) {
+        winningGames++;
+      }
+    }
+
+    return `${this.team} won ${winningGames} games.`;
+  }
 }
