16ffb25448af427d2910d36bbca80731ef8ad374
Junjie Chen <junjie.chen18@gmail.com>
2020-03-19 18:05:47 -0400

    feat(logger): define log method

--- src/logger/Html.ts
+++ src/logger/Html.ts
@@ -1,3 +1,16 @@
+import fs from 'fs';
+
 export class Html {
   constructor(public file: string) {}
+
+  log(analysis: string): void {
+    const html = `
+      <div>
+        <h1>Winning Games</h1>
+        <div>${analysis}</div>
+      </div>
+    `;
+
+    fs.writeFileSync(this.file, html);
+  }
 }
