d5226980caeac111d438b37ee3dba91c0717c205
Junjie Chen <junjie.chen18@gmail.com>
2020-03-19 18:24:17 -0400

    feat(log): create instance of Report class and assign it to report variable

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { MatchReader } from './MatchReader';
+import { Report } from './Report';
 
 const matchReader = MatchReader.createMatchReader('soccer-match.csv');
+
+const report = Report.createReport('Man United', 'report.html');
 
 matchReader.load();
