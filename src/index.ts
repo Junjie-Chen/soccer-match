914cd89752de57f8b91f7e3949bb5f0168990d66
Junjie Chen <junjie.chen18@gmail.com>
2020-03-19 17:42:13 -0400

    feat(log): analyze the number of winning games of Man United

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
 
 const matchReader = new MatchReader('soccer-match.csv');
 
 matchReader.read();
+
+let winningGames = 0;
+
+for (let match of matchReader.result) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeTeamWinsTheGame) {
+    winningGames++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayTeamWinsTheGame) {
+    winningGames++;
+  }
+}
