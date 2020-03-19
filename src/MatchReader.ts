34ad804f21109ff150c00e0e9093edeb8a53ff2f
Junjie Chen <junjie.chen18@gmail.com>
2020-03-19 17:55:48 -0400

    feat(match reader): define load method

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,5 +1,7 @@
 import { FileReader } from './FileReader';
 import { MatchType } from './MatchType';
+import { parseDate } from './utils';
+import { MatchResult } from './MatchResult';
 
 export class MatchReader {
   static createMatchReader(file: string): MatchReader {
@@ -9,4 +11,20 @@ export class MatchReader {
   matches: MatchType[] = [];
 
   constructor(public reader) {}
+
+  load(): void {
+    this.reader.read();
+
+    this.matches = this.reader.result.map((record: string[]): MatchType => {
+      return [
+        parseDate(record[0]),
+        record[1],
+        record[2],
+        parseInt(record[3]),
+        parseInt(record[4]),
+        record[5] as MatchResult,
+        record[6]
+      ];
+    });
+  }
 }
