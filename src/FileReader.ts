ffe8c9f38c0b4957f269ccc727269367921245a6
Junjie Chen <junjie.chen18@gmail.com>
2020-03-19 17:29:42 -0400

    feat(file reader): use generics for FileReader class

--- src/FileReader.ts
+++ src/FileReader.ts
@@ -1,11 +1,11 @@
 import fs from 'fs';
 
-export abstract class FileReader {
-  result = [];
+export abstract class FileReader<T> {
+  result: T[] = [];
 
   constructor(public file: string) {}
 
-  abstract parseRecord(record: string[]);
+  abstract parseRecord(record: string[]): T;
 
   read(): void {
     this.result = fs
