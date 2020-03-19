11bb483b3181032a5560c962cbd56c3c5aad36f5
Junjie Chen <junjie.chen18@gmail.com>
2020-03-19 18:18:37 -0400

    feat(report): define analyzeAndLog method

--- src/Report.ts
+++ src/Report.ts
@@ -1,6 +1,7 @@
 import { Winner } from './analyzer/Winner';
 import { Html } from './logger/Html';
 // import { Console } from './logger/Console';
+import { MatchType } from './MatchType';
 
 export class Report {
   static createReport(team: string, file: string): Report {
@@ -9,4 +10,8 @@ export class Report {
   }
 
   constructor(public analyzer, public logger) {};
+
+  analyzeAndLog(matches: MatchType[]): void {
+    this.logger.log(this.analyzer.analyze(matches));
+  }
 }
