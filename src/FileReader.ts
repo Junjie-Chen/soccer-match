3d9423610087d87fa412c74d2d07be745439d622
Junjie Chen <junjie.chen18@gmail.com>
2020-03-19 17:48:33 -0400

    feat(file reader): define read method

--- src/FileReader.ts
+++ src/FileReader.ts
@@ -1,5 +1,18 @@
+import fs from 'fs';
+
 export class FileReader {
   result: string[][] = [];
 
   constructor(public file: string) {}
+
+  read(): void {
+    this.result = fs.
+      readFileSync(this.file, {
+        encoding: 'utf-8'
+      })
+      .split('\n')
+      .map((record: string): string[] => {
+        return record.split(',');
+      });
+  }
 }
